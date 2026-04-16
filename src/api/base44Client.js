004614e96180d8b115912fafc02b0e13b2221dd9
EMSW <alamiuness@gmail.com>
2026-04-16 16:01:10 +0100

    base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,17 +1,19 @@
 import { createClient } from '@base44/sdk';
 import { appParams } from '@/lib/app-params';
 
+// Kansstakhrejo l-ma3loumat men appParams li gaddina qbel
 const { appId, token, functionsVersion, appBaseUrl } = appParams;
 
 export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  // ServerUrl khassha t-koun khawia ila kanti kheddam b Cloudflare Functions
+  // Ila malqach appId f appParams, kista3mel hada li lqiti
+  appId: appId || "69e0a9aa142ba698bfbf0177", 
+  token: token,
+  functionsVersion: functionsVersion,
   serverUrl: '', 
   requiresAuth: false,
-  appBaseUrl,
+  appBaseUrl: appBaseUrl,
   headers: {
-    "x-api-key": "7dcf7984a7d34aaeb780748c58dc0eed" // Jarreb beddel "api_key" b "x-api-key"
+    // Had l-key darouri bach l-API tqbel l-ittisal
+    "x-api-key": "7dcf7984a7d34aaeb780748c58dc0eed"
   }
 });

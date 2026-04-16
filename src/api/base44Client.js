aec7e9e4039665369214379b75a2253646038c05
EMSW <alamiuness@gmail.com>
2026-04-16 15:35:17 +0100

    base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -1,14 +1,15 @@
 import { createClient } from '@base44/sdk';
 import { appParams } from '@/lib/app-params';
 
-const { appId, token, functionsVersion, appBaseUrl } = appParams;
-
-//Create a client with authentication required
+// Gad l-client b l-ma3loumat li lqiti
 export const base44 = createClient({
-  appId,
-  token,
-  functionsVersion,
-  serverUrl: '',
+  appId: "69e0a9aa142ba698bfbf0177", // L-App ID dialek
+  token: appParams.token,
+  functionsVersion: appParams.functionsVersion,
+  serverUrl: '', 
   requiresAuth: false,
-  appBaseUrl
+  appBaseUrl: appParams.appBaseUrl,
+  headers: {
+    "api_key": "7dcf7984a7d34aaeb780748c58dc0eed" // L-API Key dialek
+  }
 });
